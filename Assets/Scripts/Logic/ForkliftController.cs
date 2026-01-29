d1b299a8e0b549dc343ae16bc5c8da1b201d1108
TehKeeper <lexmechanicum@gmail.com>
2026-01-29 17:45:12 +0300

    Game Input

--- Assets/Scripts/Logic/ForkliftController.cs
+++ Assets/Scripts/Logic/ForkliftController.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 
 namespace Logic {
-    [RequireComponent(typeof())]
-    public class ForkliftController : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Awake()
-        {
+    [RequireComponent(typeof(Rigidbody))]
+    public class ForkliftController : MonoBehaviour {
+        [SerializeField] private float _force = 100;
         
+        
+        private Rigidbody _rigidBody;
+
+        //private ForkliftInput _input;
+
+        // Start is called before the first frame update
+        void Awake() {
+            _rigidBody = GetComponent<Rigidbody>();
+            
+            
+            //_inputControl =  new Forklift
+            
         }
 
         // Update is called once per frame
